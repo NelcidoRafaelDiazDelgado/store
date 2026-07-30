85e1edbdbcf1b270451092ebba3baf6b49713901
NelcidoRafaelDiazDelgado <NelcidoRafaelDiazDelgado>
2026-07-30 22:04:24 +0000

    fix; adding dashboard layout
    changes:
    -fixing linter problems
    -adding email on sidebar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,11 @@
 class DashboardController < ApplicationController
   include Pagy::Backend
 
+  layout "dashboard"
   def index
       @pagy, @products = pagy(Product
       .order(updated_at: :desc))
+
+      @email = Current.user.email_address
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
   include Pagy::Backend
+  layout "dashboard"
 
   # GET /products or /products.json
   def index
